1871bad70e50f64a8999763a895f3b4d0240842b
Fabio Blonda <blofabio@amazon.it>
2025-04-02 16:37:43 +0200

    aws configs fix attempt 10

--- src/components/Game/component.js
+++ src/components/Game/component.js
@@ -10,14 +10,14 @@ import Video from '../Video';
 import Modal from '../Modal';
 import styles from './styles';
 import awsmobile from './../../aws-exports';
-const awsconfig = {
+/*const awsconfig = {
     aws_appsync_graphqlEndpoint: "https://sslhe7ftqvdz3f7w5uhjjeobdm.appsync-api.eu-west-1.amazonaws.com/graphql",
     aws_appsync_region: "eu-west-1",
     aws_appsync_authenticationType: 'API_KEY',
     aws_appsync_apiKey: "da2-nirzx2l2zfgr7d5avqtap3hx3a"
-};
+};*/
 
-Amplify.configure(awsconfig);
+Amplify.configure(awsmobile);
 
 class Game extends Component {
 	constructor(props){
