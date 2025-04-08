8fa820969c841049debbf10f147d5a63c213eb3c
Fabio Blonda <blofabio@amazon.it>
2025-04-08 13:23:53 +0200

    fix awsconfig 8

--- src/components/Game/component.js
+++ src/components/Game/component.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { Amplify } from 'aws-amplify';
-import awsmobile from '../../aws-exports';
+import awsmobile from '../../aws-exports2';
 import { API, graphqlOperation } from 'aws-amplify';
 import { print as gqlToString } from 'graphql/language';
 import { onCreateQuestion, onUpdateQuestion } from '../../graphql/subscriptions';
@@ -11,14 +11,8 @@ import Video from '../Video';
 import Modal from '../Modal';
 import styles from './styles';
 
-const awsconfig = {
-    aws_appsync_graphqlEndpoint: "https://sslhe7ftqvdz3f7w5uhjjeobdm.appsync-api.eu-west-1.amazonaws.com/graphql",
-    aws_appsync_region: "eu-west-1",
-    aws_appsync_authenticationType: 'API_KEY',
-    aws_appsync_apiKey: "da2-nirzx2l2zfgr7d5avqtap3hx3a"
-};
 
-Amplify.configure(awsconfig);
+Amplify.configure(awsmobile);
 
 class Game extends Component {
 	constructor(props){
@@ -48,9 +42,9 @@ class Game extends Component {
 
 	componentDidMount(){
 		try {
-			console.log('AppSync Config:', awsconfig);
+			console.log('AppSync Config:', awsmobile);
 			// Configure Amplify for this component
-			Amplify.configure(awsconfig);
+			Amplify.configure(awsmobile);
 			this.listenForQuestions();
 			this.listenForAnswers();
 		} catch (error) {
